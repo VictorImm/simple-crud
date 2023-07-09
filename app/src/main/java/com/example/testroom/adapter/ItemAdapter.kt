ca2e21838293234966b2d9fada8bd38509841b24
VictorImm <victorimmanuelss@gmail.com>
2023-07-10 01:58:27 +0700

    [UPDATE] add click listener

--- app/src/main/java/com/example/testroom/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/testroom/adapter/ItemAdapter.kt
@@ -1,16 +1,18 @@
 package com.example.testroom.adapter
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.testroom.ItemDetail
 import com.example.testroom.R
+import com.example.testroom.data.Item
 
-class ItemAdapter(private val listItem: ArrayList<String>): RecyclerView.Adapter<ItemAdapter.ListViewHolder>() {
+class ItemAdapter(private val listItem: List<Item>): RecyclerView.Adapter<ItemAdapter.ListViewHolder>() {
     class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val tvItem: TextView = itemView.findViewById(R.id.item)
+        val tvItem: TextView = itemView.findViewById(R.id.item_name)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
@@ -28,6 +30,13 @@ class ItemAdapter(private val listItem: ArrayList<String>): RecyclerView.Adapter
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val item = listItem[position]
-        holder.tvItem.text = item
+        holder.tvItem.text = item.itemName
+
+        // set if list is clicked
+        holder.itemView.setOnClickListener{
+            val intentDetail = Intent(holder.itemView.context, ItemDetail::class.java)
+            intentDetail.putExtra("item_id", item.id)
+            holder.itemView.context.startActivity(intentDetail)
+        }
     }
 }
