651e611329515627df723e74602822a820797806
VictorImm <victorimmanuelss@gmail.com>
2023-07-10 01:58:48 +0700

    [UPDATE] implement room for db

--- app/src/main/java/com/example/testroom/MainActivity.kt
+++ app/src/main/java/com/example/testroom/MainActivity.kt
@@ -3,25 +3,29 @@ package com.example.testroom
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import android.widget.Button
+import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.testroom.adapter.ItemAdapter
+import com.example.testroom.data.*
 import com.example.testroom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var rvItems: RecyclerView
+    // binding
     private lateinit var binding: ActivityMainBinding
 
-    private val items = ArrayList<String>(listOf(
-        "item 1",
-        "item 2",
-        "item 3",
-        "item 4",
-        "item 5",
-    ))
+    // widgets
+    private lateinit var rvItems: RecyclerView
+
+    // property initialization
+    private val viewModel: ItemViewModel by viewModels {
+        ItemViewModelFactory(
+            (application as ItemApplication).database.itemDao()
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,12 +34,15 @@ class MainActivity : AppCompatActivity() {
 
         rvItems = binding.rvItems
         rvItems.setHasFixedSize(true)
-        showRecyclerList()
+
+        viewModel.allItems.observe(this) { items ->
+            showRecyclerList(items)
+        }
 
         intentManager()
     }
 
-    private fun showRecyclerList() {
+    private fun showRecyclerList(items: List<Item>) {
         rvItems.layoutManager = LinearLayoutManager(this)
         // show recycle view from selected month
         val itemAdapter = ItemAdapter(items)
@@ -44,7 +51,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun intentManager() {
         // button to add daily expense
-        val btnAdd: Button = findViewById<Button>(R.id.btnAdd)
+        val btnAdd: Button = findViewById(R.id.btnAdd)
         btnAdd.setOnClickListener {
             val intent = Intent(this, AddItem::class.java)
             startActivity(intent)
