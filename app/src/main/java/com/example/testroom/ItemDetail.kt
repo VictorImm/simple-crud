3467523bfb0234b5a1343db1ac69bbedbb6084bf
VictorImm <victorimmanuelss@gmail.com>
2023-07-11 17:13:05 +0700

    [UPDATE] binding update n delete

--- app/src/main/java/com/example/testroom/ItemDetail.kt
+++ app/src/main/java/com/example/testroom/ItemDetail.kt
@@ -1,13 +1,18 @@
 package com.example.testroom
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.core.content.ContentProviderCompat.requireContext
+import com.example.testroom.data.ItemDao
 import com.example.testroom.data.ItemViewModel
 import com.example.testroom.data.ItemViewModelFactory
 import com.example.testroom.databinding.ActivityItemDetailBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class ItemDetail : AppCompatActivity() {
 
@@ -16,6 +21,8 @@ class ItemDetail : AppCompatActivity() {
 
     // widgets
     private lateinit var tvTextView: TextView
+    private lateinit var btnUpdate: Button
+    private lateinit var btnDelete: Button
 
     // property initialization
     private val viewModel: ItemViewModel by viewModels {
@@ -32,10 +39,51 @@ class ItemDetail : AppCompatActivity() {
         binding = ActivityItemDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        tvTextView = binding.textView
+        tvTextView = binding.itemName
 
         viewModel.retrieveItem(itemId).observe(this) { selectedItem ->
-            tvTextView.text = selectedItem.itemName
+            if (selectedItem != null) {
+                tvTextView.text = selectedItem.itemName
+            }
+            else {
+                tvTextView.text = "deleted item"
+            }
         }
+
+        updateItem(itemId)
+        deleteItem(itemId)
+    }
+
+    private fun updateItem(itemId: Int) {
+        btnUpdate = binding.btnUpdate
+
+        // set click listener
+        btnUpdate.setOnClickListener {
+            val intent = Intent(this, AddItem::class.java)
+            intent.putExtra("item_id", itemId)
+            startActivity(intent)
+
+            // finish activity
+            // finish()
+        }
+    }
+
+    private fun deleteItem(itemId: Int): Int {
+        btnDelete = binding.btnDelete
+
+        // set click listener
+        btnDelete.setOnClickListener {
+            // TODO: create dialogue
+
+            viewModel.retrieveItem(itemId).observe(this) { selectedItem ->
+                if (selectedItem != null) {
+                    viewModel.deleteItem(selectedItem)
+                }
+            }
+
+            // finish activity
+            finish()
+        }
+        return 0
     }
 }
